049750e622cdefa4c7f39b69ebebeb847728d4ca
Andrey Oleynik <andrey.oleynik@corp.mail.ru>
2019-03-28 00:39:47 +0300

    Make better animation

--- vk/FriendInfo/FriendInfoCell.swift
+++ vk/FriendInfo/FriendInfoCell.swift
@@ -30,6 +30,11 @@ class FriendInfoCell: UICollectionViewCell {
 }
 
 extension FriendInfoCell: UIGestureRecognizerDelegate {
+    enum Direction {
+        case right
+        case left
+    }
+    
     @objc func swiped(gesture: UIGestureRecognizer) {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
             guard let currentIndex = delegate?.getFriendCurrentImageIndex(), let imagesCount = delegate?.getFriendImagesCount() else {
@@ -42,18 +47,16 @@ extension FriendInfoCell: UIGestureRecognizerDelegate {
                 if currentIndex < imagesCount - 1 {
                     let newIndex = currentIndex + 1
                     let image = delegate?.getFriendImage(id: newIndex)
-                    avatar.image = image
                     delegate?.updateFriendCurrentImageIndex(id: newIndex)
-                    animateImage()
+                    animateImage(direction: .left, image: image)
                 }
             case .right:
                 print("swiped right")
                 if currentIndex > 0 {
                     let newIndex = currentIndex - 1
                     let image = delegate?.getFriendImage(id: newIndex)
-                    avatar.image = image
                     delegate?.updateFriendCurrentImageIndex(id: newIndex)
-                    animateImage()
+                    animateImage(direction: .right, image: image)
                 }
             default:
                 break
@@ -61,17 +64,34 @@ extension FriendInfoCell: UIGestureRecognizerDelegate {
         }
     }
     
-    func animateImage() {
-        UIView.transition(with: self, duration: 1, options: .transitionCrossDissolve, animations: {
-            self.contentView.transform = CGAffineTransform(scaleX: 0.6, y: 0.8)
-        })
-
-        let right = CGAffineTransform(translationX: 300, y: 0)
-        let left = CGAffineTransform(translationX: -300, y: 0)
-        UIView.animate(withDuration: 1) {
-            self.contentView.transform = right
+    func animateImage(direction: Direction, image: UIImage?) {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            self.avatar.image = image
         }
-        self.contentView.transform = left
+        
+        UIView.animateKeyframes(withDuration: 1.52, delay: 0, options: .calculationModeCubic, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.3) {
+                self.avatar.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.001, animations: {
+                self.avatar.transform = .identity
+                if direction == .right {
+                    self.avatar.center.x -= 300
+                } else {
+                    self.avatar.center.x += 300
+                }
+            })
+       
+            UIView.addKeyframe(withRelativeStartTime: 0.51, relativeDuration: 0.5) {
+                if direction == .right {
+                    self.avatar.center.x += 300
+                } else {
+                    self.avatar.center.x -= 300
+                }
+            }
+        })
     }
 }
 
