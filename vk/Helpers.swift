643861e3acf79e05fea31661aa01ca17fe8407d1
Andrey Oleynik <andrey.oleynik@corp.mail.ru>
2019-06-15 01:05:26 +0300

    Use NSOperation

--- vk/Helpers.swift
+++ vk/Helpers.swift
@@ -54,3 +54,98 @@ extension UIImageView {
         downloaded(from: url, contentMode: mode)
     }
 }
+
+func blur(image: UIImage) -> UIImage {
+    let inputCIImage = CIImage(image: image)!
+    
+    let blurFilter = CIFilter(name: "CIGaussianBlur", parameters: [kCIInputImageKey: inputCIImage])!
+    let outputImage = blurFilter.outputImage!
+    let context = CIContext()
+    let cgiImage = context.createCGImage(outputImage, from: outputImage.extent)
+    
+    let bluredImage = UIImage(cgImage: cgiImage!)
+    return bluredImage
+}
+
+class AsyncOperation: Operation {
+    enum State: String {
+        case ready, executing, finished
+        fileprivate var keyPath: String {
+            return "is" + rawValue.capitalized
+        }
+    }
+    
+    var state = State.ready {
+        willSet {
+            willChangeValue(forKey: state.keyPath)
+            willChangeValue(forKey: newValue.keyPath)
+        }
+        didSet {
+            didChangeValue(forKey: state.keyPath)
+            didChangeValue(forKey: oldValue.keyPath)
+        }
+    }
+    
+    override var isAsynchronous: Bool {
+        return true
+    }
+    
+    override var isReady: Bool {
+        return super.isReady && state == .ready
+    }
+    
+    override var isExecuting: Bool {
+        return state == .executing
+    }
+    override var isFinished: Bool {
+        return state == .finished
+    }
+    override func start() {
+        if isCancelled {
+            state = .finished
+        } else {
+            main()
+            state = .executing
+        }
+    }
+    override func cancel() {
+        super.cancel()
+        state = .finished
+    }
+}
+
+class GetImageOperation: AsyncOperation {
+    private var imageUri: String
+    var image: UIImage?
+    
+    init(image_uri: String) {
+        self.imageUri = image_uri
+    }
+    
+    override func main() {
+        URLSession.shared.dataTask(with: URL(string: imageUri)!) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data) else { return }
+            
+            self.image = blur(image: image)
+            self.state = .finished
+            
+            }.resume()
+    }
+}
+
+class SetImageOperation: Operation {
+    var imageView: UIImageView
+    
+    init(imageView: UIImageView) {
+        self.imageView = imageView
+    }
+    
+    override func main() {
+        guard let op = dependencies.first as? GetImageOperation else { return }
+        imageView.image = op.image
+    }
+}
